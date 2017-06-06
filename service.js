f0110d51fee9d86b8de88fce0c974f8c53775dfd
Waly <debianw@gmail.com>
2017-06-06 13:05:34 -0600

    port change

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
 const os = require('os');
 const express = require('express');
 const app = express();
-const port = 9000;
+const port = 9001;
 
 app.get('/micro-2/test', (req, res) => {
   res.json({ 
